beef2e738ce6ae348a97d8a0dee591855c8b83f8
Jeff Fredenburg <jfreden@fnal.gov>
2025-02-26 07:21:46 -0600

    rewrite tests for the decorator class so the mock classes do not need special ruff flags to disable unused arguments

--- test/labtest/test_deocrator/test_decorator_labtest.py
+++ test/labtest/test_deocrator/test_decorator_labtest.py
@@ -37,7 +37,7 @@ def test_decorator_labtest_no_argument(monkeypatch):
     registry = Registry(is_singleton=False)
 
     class MockRegistry(Registry):
-        def __new__(cls, *, is_singleton: bool = True):  # noqa: ARG003
+        def __new__(cls, *_):
             return registry
 
     monkeypatch.setattr(labtest.decorator, "Registry", MockRegistry)
